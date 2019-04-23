a96d6a53f53ec853500880b98eae4add31cb091c
245545357@qq.com <245545357@qq.com>
2019-04-23 14:41:13 +0800

    代码优化

--- app/src/main/java/com/mmo/tooldemo/MainActivity.java
+++ app/src/main/java/com/mmo/tooldemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mmo.tooldemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity
@@ -12,6 +14,15 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("allen","123");
+
+        @SuppressLint("MissingPermission") String imei = ((TelephonyManager) this.getSystemService(TELEPHONY_SERVICE)).getDeviceId();
+        Log.d("allen", "imei211:");
+
+        Log.d("allen", "imei:" + imei);
+
+
+        @SuppressLint("MissingPermission") String android_imsi = ((TelephonyManager) this.getSystemService(TELEPHONY_SERVICE)).getSubscriberId();
+        Log.d("allen", "imsi:" + android_imsi);
+
     }
 }
