b83e030916943b9ea40424528d96975993b49479
245545357@qq.com <245545357@qq.com>
2019-08-20 16:37:13 +0800

    测试andromeda组件通信框架

--- commonTool/src/main/java/com/andr/common/tool/log/LoggerUtil.java
+++ commonTool/src/main/java/com/andr/common/tool/log/LoggerUtil.java
@@ -2,6 +2,7 @@ package com.andr.common.tool.log;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.CsvFormatStrategy;
@@ -18,48 +19,58 @@ public class LoggerUtil
     private static String TAG_NAME = "LoggerTag";
     private static boolean IS_CACHE = false;     //true表示缓存日志到文件
     private static boolean IS_LOGGABLE = false;     //true表示输出Log
+    private static int logLv = 0;
 
     /**
      * 日志系统初始化
-     * @param Tag 打印的Tag
+     *
+     * @param Tag     打印的Tag
      * @param openLog 是否打开log
-     * @param saveLog 是否保存log
+     * @param logLeve 要保存的日志等级信息,3是d,4是i,5是w,6是e
      */
-    public static void initLogger(String Tag, boolean openLog, boolean saveLog) {
+    public static void initLogger(String Tag, boolean openLog, int logLeve)
+    {
 
         TAG_NAME = Tag;
         IS_LOGGABLE = openLog;
-        IS_CACHE = saveLog;
-
+        logLv = logLeve;
 
         init();
     }
 
 
-    private static void init() {
+    private static void init()
+    {
 
 
         if (!IS_LOGGABLE)   // 保存Log信息到File文件中,文件路径 /sdcard/logger
         {
             //DiskLogAdapter 添加配置
             FormatStrategy formatStrategy = CsvFormatStrategy.newBuilder()
-//                    .date(new Date())  // 设置保存的时间，默认data();
+                    //                    .date(new Date())  // 设置保存的时间，默认data();
                     .dateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"))  // 设置保存的格式化时间,默认 yyyy.MM.dd HH:mm:ss.SSS
-//                    .logStrategy()  // 更改日志策略以打印出。默认的logcat
+                    //                    .logStrategy()  // 更改日志策略以打印出。默认的logcat
                     .tag(TAG_NAME)  // 自定义日志标记
                     .build();
             Logger.addLogAdapter(new DiskLogAdapter(formatStrategy)
             {
                 @Override
-                public boolean isLoggable(int priority, @Nullable String tag) {
-                    return IS_CACHE;
+                public boolean isLoggable(int priority, @Nullable String tag)
+                {
+                    Log.d("allen", "等级:" + priority);
+
+                    if (priority > logLv)
+                    {
+                        return !IS_LOGGABLE;
+
+                    }
+                    return IS_LOGGABLE;
                 }
             });
         } else   // 不保存Log信息
         {
             //AndroidLogAdapter 添加的配置
-            FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
-                    .showThreadInfo(false)  // (Optional) Whether to show thread info or not. Default true
+            FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder().showThreadInfo(false)  // (Optional) Whether to show thread info or not. Default true
                     // 是否显示线程消息，默认为true
                     .methodCount(0)         // (Optional) How many method line to show. Default 2
                     // 要显示多少个方法行。默认值为2
@@ -73,7 +84,8 @@ public class LoggerUtil
             Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy)
             {
                 @Override
-                public boolean isLoggable(int priority, @Nullable String tag) {
+                public boolean isLoggable(int priority, @Nullable String tag)
+                {
                     return IS_LOGGABLE;
                 }
             });
@@ -81,28 +93,40 @@ public class LoggerUtil
 
     }
 
-    public static void d(@Nullable Object object) {
+    public static void d(@Nullable Object object)
+    {
         Logger.d(object);
     }
 
 
-    public static void i(@NonNull String message, @Nullable Object... args) {
+    public static void i(@NonNull String message, @Nullable Object... args)
+    {
         Logger.i(message, args);
     }
 
-    public static void d(@NonNull String message, @Nullable Object... args) {
+    public static void w(@NonNull String message, @Nullable Object... args)
+    {
+        Logger.w(message, args);
+    }
+
+    public static void d(@NonNull String message, @Nullable Object... args)
+    {
         Logger.d(message, args);
     }
 
-    public static void e(@NonNull String message, @Nullable Object... args) {
+    public static void e(@NonNull String message, @Nullable Object... args)
+    {
         Logger.e(message, args);
     }
 
-    public static void json(String json) {
+    public static void json(String json)
+    {
         Logger.json(json);
 
     }
-    public static void xml(String xml) {
+
+    public static void xml(String xml)
+    {
         Logger.xml(xml);
 
     }
